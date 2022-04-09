c6779ef94bac5524686f6933258c2ce51ce2bd50
siam666-shell <abdullahsiam009@gmail.com>
2022-04-10 04:47:49 +0600

    edit issue done from backend

--- src/EditIssue.jsx
+++ src/EditIssue.jsx
@@ -4,6 +4,10 @@ import { useState, useEffect, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { IssueContext } from './context/IssueContext';
+import axiosAPI from './utils/axiosAPI';
+import useToken from './hooks/useToken';
+import formatIssue from './utils/formatIssue';
+import { parseISO } from 'date-fns/esm';
 
 const defaultIssue = {
   title: '',
@@ -21,19 +25,40 @@ const EditIssue = () => {
   const { issues, updateIssue } = useContext(IssueContext);
   const navigate = useNavigate();
   const { id } = useParams();
+  const { token, tokenLoaded } = useToken();
 
-  const issueToEdit = () => {
-    const foundIssue = issues.find((issue) => issue.id === id);
-    if (!foundIssue) {
-      toast.error('Issue is not found to be updated');
-      return navigate('/issues');
-    }
-    setIssue(foundIssue);
+  const issueToEdit = async () => {
+    const data = await axiosAPI({
+      method: 'get',
+      url: `/issues/${+id}`,
+      config: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    });
+
+    const issue = formatIssue(data.data);
+    console.log(issue);
+
+    // const foundIssue = issues.find((issue) => issue.id === id);
+    // if (!foundIssue) {
+    //   toast.error('Issue is not found to be updated');
+    //   return navigate('/issues');
+    // }
+    setIssue({
+      ...issue,
+      startDate: parseISO(issue.startDate),
+      endDate: parseISO(issue.endDate),
+      assignTo: '...',
+    });
   };
 
   useEffect(() => {
-    issueToEdit();
-  }, [id]);
+    if (token && tokenLoaded) {
+      issueToEdit();
+    }
+  }, [id, tokenLoaded, token]);
 
   const handleUpdateIssue = (issue) => {
     updateIssue(issue);
